733548730602f6ca70403b7b4e844bc4786b0843
“robin-31” <rm.robinmanna01@gmail.com>
2023-12-05 23:45:55 +0530

    add code to new branch

---
+++ java_logic_doubt/src/java_logic_doubt_test/NewBranch.java
@@ -0,0 +1,11 @@
+package java_logic_doubt_test;
+
+public class NewBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello! switched to new branch");
+
+	}
+
+}
